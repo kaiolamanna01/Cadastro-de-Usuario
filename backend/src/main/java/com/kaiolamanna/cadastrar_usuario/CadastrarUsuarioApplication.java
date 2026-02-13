911471d60147b9f2e4ea5b45ea188000b4db0837
Kaio <kaio.lamanna5@gmail.com>
2026-02-13 15:15:30 -0300

    Fazendo Backend raiz do Back

---
+++ backend/src/main/java/com/kaiolamanna/cadastrar_usuario/CadastrarUsuarioApplication.java
@@ -0,0 +1,13 @@
+package com.kaiolamanna.cadastrar_usuario;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CadastrarUsuarioApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CadastrarUsuarioApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/kaiolamanna/cadastrar_usuario/CadastrarUsuarioApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kaiolamanna.cadastrar_usuario;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CadastrarUsuarioApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
