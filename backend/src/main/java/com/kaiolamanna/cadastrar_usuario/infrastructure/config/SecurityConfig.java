9686b3c38a7ad9b8cfbd6d4f8be24721267a190b
Kaio <kaio.lamanna5@gmail.com>
2026-02-25 21:19:54 -0300

    Corrigindo SecurityConfig

--- backend/src/main/java/com/kaiolamanna/cadastrar_usuario/infrastructure/config/SecurityConfig.java
+++ backend/src/main/java/com/kaiolamanna/cadastrar_usuario/infrastructure/config/SecurityConfig.java
@@ -28,7 +28,7 @@ public class SecurityConfig {
             .cors(cors -> cors.configurationSource(corsConfigurationSource()))
             .sessionManagement(s -> s.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
             .authorizeHttpRequests(auth -> auth
-                .requestMatchers("/auth/login").permitAll()
+                .requestMatchers("/auth/login", "/usuario").permitAll()
                 .anyRequest().authenticated()
             )
             .addFilterBefore(jwtFilter, UsernamePasswordAuthenticationFilter.class);
